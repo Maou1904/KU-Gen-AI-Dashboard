4283d5dd247d82d256290344153848161c202f6d
Maou1904 <maoupenmha@gmail.com>
2026-07-04 22:41:46 +0700

    cleaning folders

--- tmp/wall_counts_20260630/list_floor_lines.mjs
+++
@@ -1,9 +0,0 @@
-import fs from "node:fs/promises";
-
-const text = await fs.readFile("C:/Users/atipo/Downloads/text.txt", "utf8");
-for (const line of text.split(/\r?\n/)) {
-  const trimmed = line.trim();
-  if (trimmed.startsWith("ระดับ")) {
-    console.log(trimmed);
-  }
-}
